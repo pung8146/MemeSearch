692937d93bf3cf5c5b876343d1bb5003022441b2
pung8146 <pung8146@naver.com>
2024-08-11 16:35:04 +0900

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/popup.html"),
+        content: resolve(__dirname, "src/content.ts"),
+        background: resolve(__dirname, "src/background.ts"),
+      },
+      output: {
+        entryFileNames: "[name].js",
+        chunkFileNames: "[name].js",
+        assetFileNames: "[name].[ext]",
+      },
+    },
+  },
+});
